1accb5a608b20caceed0deb3e7088fc03a27ec3d
MinGook <mgk8609@naver.com>
2021-07-31 12:09:03 +0900

    CBV로 블로그 글 상세페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.PostList.as_view())
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    # path('<int:pk>/', views.single_post_page),
     # path('', views.index),  FBV 방식에서 사용하던 함수
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 # CBV 방식으로 페이지 만들기, 반복되는 기능은 이미 정의해놓은 class가 있을때가 많다. CBV를 활용해보자.
-from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 
 class PostList(ListView):
-    ordering = '-pk'
     model = Post
+    ordering = '-pk'
 
+
+class PostDetail(DetailView):
+    model = Post
 
 # FBV 방식으로 페이지 만든 것
 
@@ -25,14 +28,14 @@ class PostList(ListView):
 #             'posts': posts,
 #         }
 #     )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
