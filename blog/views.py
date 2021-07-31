d65927af91a5dec8ddb4f2828a92bbfb2a2f68eb
MinGook <mgk8609@naver.com>
2021-07-31 12:02:36 +0900

    CBV로 블로그 글 목록 바꿔보기

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,30 @@
+# CBV 방식으로 페이지 만들기, 반복되는 기능은 이미 정의해놓은 class가 있을때가 많다. CBV를 활용해보자.
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+
+class PostList(ListView):
+    ordering = '-pk'
+    model = Post
 
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# FBV 방식으로 페이지 만든 것
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )    
+# from django.shortcuts import render
+# from .models import Post
+#
+#
+# # Create your views here.
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
